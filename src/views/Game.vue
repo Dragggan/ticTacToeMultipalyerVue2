765fb52032ac597f9260a0e281efa2f4ea040726
Djuza, Dragan <dragan.djuza@centogene.com>
2022-01-19 21:40:37 +0100

    added vuetify, sucesful request

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -7,7 +7,11 @@ import axios from 'axios';
 export default {
   name: 'Game',
   async mounted() {
-    await axios.get;
+    console.log('%c  $==> ', 'color:red;font-size:12px;', process.env.VUE_APP_API_KEY);
+    await axios
+      .post(`${process.env.VUE_APP_BASE_URL}/player`, { name: 'test1', apikey: process.env.VUE_APP_API_KEY })
+      .then(response => console.log('%c  response==> ', 'color:red;font-size:12px;', response))
+      .catch(err => console.log('%c  err==> ', 'color:red;font-size:12px;', err));
   }
 };
 </script>
